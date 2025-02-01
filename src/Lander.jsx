4183518e9ebb4bbf24a43fa60ce20d0909380c0b
DerekY2 <derek.yu869@gmail.com>
2025-02-01 00:39:28 -0500

    fix Navbar import

--- src/Lander.jsx
+++ src/Lander.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './Lander.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 
 function Lander() {
   const [count, setCount] = useState(0)
